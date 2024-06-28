114e36bedb61609555b8b8b5d7c735422b911594
Egor Zviagintcev <egor.zvyagintsev@gmail.com>
2024-06-28 20:35:24 +0300

    Add allure results

--- conftest.py
+++ conftest.py
@@ -4,20 +4,14 @@ from selenium import webdriver
 from user_api.stellar_burger_api import User
 
 
-@pytest.fixture(scope='class')
-def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.implicitly_wait(10)
+@pytest.fixture(params=["chrome", "firefox"])
+def driver(request):
+    if request.param == "chrome":
+        driver = webdriver.Chrome()
+    elif request.param == "firefox":
+        driver = webdriver.Firefox()
     yield driver
     driver.quit()
-
-
-@pytest.fixture(scope='function', autouse=True)
-def clean_cookies(driver):
-    driver.delete_all_cookies()
-    yield
-    driver.delete_all_cookies()
 
 
 @pytest.fixture(scope='function')
@@ -26,17 +20,3 @@ def user():
     user.create_user()
     yield user
     user.delete_user()
-
-'''
-@pytest.fixture(params=["chrome", "firefox"])
-def driver(request):
-    if request.param == "chrome":
-        driver = webdriver.Chrome()
-    elif request.param == "firefox":
-        driver = webdriver.Firefox()
-
-    driver.get(BASE_URL)
-    yield driver
-
-    driver.quit()
-'''
